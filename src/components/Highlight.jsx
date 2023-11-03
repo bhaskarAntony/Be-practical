5545fb0b316f28aac9adb3fc40cc71db3d9803b5
bhaskarAntony <rooparoopa7105@gmail.com>
2023-11-03 19:27:59 +0530

    home page highlight card is updated

--- src/components/Highlight.jsx
+++ src/components/Highlight.jsx
@@ -4,24 +4,24 @@ import '../styles/highlight.css'
 function Highlight({data}) {
   return (
     <div className="p-2">
-      <div className='container highlight bg-dark-gray mb-4 py-4'>
+      <div className='container highlight bg-dark-gray mb-4 py-2'>
       <div className="blur-highlight">
 
       </div>
       <div className="row">
-        <div className="col-4">
+        <div className="col-12 col-md-4 col-lg-4 mb-3">
             <div className="highlight-card">
                 <h1 className='fs-1 text-900 text-white'>{data[0]?.rating?data[0]?.rating:"4.7"} <i class="bi bi-star-fill text-yellow"></i></h1>
                 <p className='p-dark-small text-white'><small>Google Rating </small></p>
             </div>
         </div>
-        <div className="col-4">
+        <div className="col-12 col-md-4 col-lg-4 mb-3">
             <div className="highlight-card ">
             <h1 className='fs-1 text-900 text-white'>{data[0]?.partners? data[0]?.partners:"300"} +</h1>
                 <p className='p-dark-small text-white'><small>Hiring Partners</small></p>
             </div>
         </div>
-        <div className="col-4">
+        <div className="col-12 col-md-4 col-lg-4 mb-3">
             <div className="highlight-card">
             <h1 className='fs-1 text-900 text-white'>{data[0]?.minSalary? data[0]?.minSalary:"7.8"} LPA</h1>
                 <p className='p-dark-small text-white'><small>average dream job CTC</small></p>
