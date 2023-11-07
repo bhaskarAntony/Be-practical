9718d0f93fce747fcdebb079ace2f045d45d6fa9
bhaskarAntony <rooparoopa7105@gmail.com>
2023-11-07 16:46:20 +0530

    reduced loading time

--- src/components/Module.jsx
+++ src/components/Module.jsx
@@ -24,9 +24,9 @@ const Module = (props) => {
           key={index}
         >
           <div className="faq-question d-flex align-items-center" onClick={() => toggleAccordion(index)}>
-            <span className="count p-2 rounded-1 text-900 fs-4 text-green">{index+1}
+            <span className="count p-2 rounded-1 text-900 fs-4 text-green mx-3">{index+1}
             </span>
-            <span className="faq-question-text text-black"><span className="fw-bold  mx-2 text-main-danger"> Module{index + 1}:</span> <span className="text-white text-900">{item.title}</span></span>
+            <span className="faq-question-text text-black"><span className="fw-bold text-main-danger"> Module{index + 1}:</span> <span className="text-white text-900">{item.title}</span></span>
             <div className="faq-icon">
               {activeIndex === index ? <i class="bi bi-caret-up-fill"></i>:<i class="bi bi-caret-down-fill"></i>}
             </div>
