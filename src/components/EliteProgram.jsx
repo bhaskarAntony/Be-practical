fbffdb89920d7930ecfb25a29ed1f9669a6e90de
bhaskarAntony <rooparoopa7105@gmail.com>
2023-11-15 14:19:12 +0530

    updated files

--- src/components/EliteProgram.jsx
+++ src/components/EliteProgram.jsx
@@ -1,36 +1,55 @@
 import React from 'react'
 import '../styles/eliteProgram.css'
 import ApplyNow from '../Extra/ApplyNow'
+import dsa from '../images/browser.gif'
+import refund from '../images/cashback.gif'
+import skills from '../images/analytical-skill.gif'
+import internship from '../images/hiring.gif'
+import assistance from '../images/call-service.gif'
+import addOnce from '../images/html (1).gif'
+import { icon } from '@fortawesome/fontawesome-svg-core'
+
+
 
 const eliteProgram = [
     {
         title:"Refund",
-        content:"If not placed 50% of course fee will be given back"
+        content:"If not placed 50% of course fee will be given back",
+        icon: refund ,
     },
     {
         title:"DSA & DSA (Exclusive Problems)",
-        content:"Will be providing weekly twice"
+        content:"Will be providing weekly twice",
+        icon: dsa ,
     },
     {
         title:"Softskills",
-        content:"Soft skill refer to the personal attributes and qualities that enable effective communication."
+        content:"Soft skill refer to the personal attributes and qualities that enable effective communication.",
+        icon: skills ,
+
     },
     {
         title:"Internship",
-        content:"Internship is a short-term work experience that provides practical training and exposure."
+        content:"Internship is a short-term work experience that provides practical training and exposure.",
+        icon: internship ,
+
     },
     {
         title:"Placement Assistance",
-        content:"Unlimited Interview's"
+        content:"Unlimited Interview's",
+        icon: assistance ,
+
     },
     {
         title:"Add On's",
-        content:"AWS Basics, DevOps tools, Jenkins, Git (Starting with Python)"
+        content:"AWS Basics, DevOps tools, Jenkins, Git (Starting with Python)",
+        icon: addOnce ,
+
     },
 ]
 function EliteProgram() {
   return (
-    <div className='mt-5'>
+    <div className='mt-5 bg-blue-light'>
          <h1 className="fs-1 text-900 text-center">What is Elite Program?</h1>
          <section className='elite-progarm container py-5'>
        
@@ -39,7 +58,8 @@ function EliteProgram() {
              eliteProgram.map((item, index)=>(
                  <div className="col-12 col-md-6 col-lg-4">
                      <div className="elite-program-card">
-                         <h1 className="card-heading text-black  mb-3 text-900"><i class="bi bi-stars text-main-danger"></i> {item.title}</h1>
+                        <img src={item.icon} alt="icon" />
+                         <h1 className="card-heading text-black  mb-3 text-900">{item.title}</h1>
                          <p className="p-large1 text-secondary">{item.content}</p>
                      </div>
                   </div>
