d74088272d81932546ad7835ec94b8ee2ca520e3
bpdeveloperscommunity <bepracticaldeveloperscommunity@gmail.com>
2024-02-19 17:15:55 +0530

    updated file

--- src/pages/BlogsPage/BlogsPage.jsx
+++ src/pages/BlogsPage/BlogsPage.jsx
@@ -7,6 +7,7 @@ import { Link } from 'react-router-dom';
 import BookDemoCard from '../../components/BookDemoCard';
 import { BlogsApi } from '../../Data/DataFetcher';
 import ReadMore from '../../Extra/ReadMore';
+import { Shimmer } from 'react-shimmer';
 
 function BlogsPage() {
     
@@ -22,7 +23,7 @@ function BlogsPage() {
       }, []);
 
     const settings = {
-        dots: true,
+        dots: false,
         infinite: true,
         // speed: 1000,
         slidesToShow: 1,
@@ -32,71 +33,86 @@ function BlogsPage() {
     };
 
     return (
-        <section className='overflow-hidden'>
+        <section className='bg-light overflow-hidden'>
             <div className="blogs-hero container-fluid">
-                <Slider {...settings}>
-                    {BlogsData.map((item, index) => (
-                        <div key={index} className="slider-item">
-                            <div className="row  align-items-center">
-                                <div className="col-12 col-md-6 col-lg-6">
-                                    <div className="blogs-left p-lg-5 p-3">
-                                        
-                                            <p className="fs-5 tag p-2 text-white rounded-2"> #{item.tag}</p>
-                                        <h1 className="heading text-900">{item.title}</h1>
-                                        <p className="subtitle"><ReadMore text={item.subtitle} maxLength={100}/></p>
-                                            <div className="d-flex flex-wrap gap-2">
-                                            <p className="fs-5"> <i className="bi bi-calendar3 text-yellow mx-2"></i> {new Date(item.Date).toLocaleTimeString()}</p>
-                                    <p className="fs-5"><i className="bi bi-alarm text-green mx-2"></i>{new Date(item.Date).toDateString()}</p>
-
-                                            </div>
-                                        {/* Add other content for the left side */}
-                                    </div>
-                                </div>
-                                <div className="col-12 col-md-6 col-lg-6">
-                                    <div className="blogs-right p-lg-5 p-3">
-                                        <img src={item.image} alt={item.title} className='w-100 rounded-5 border-a p-2' />
-                                        {/* Add other content for the right side */}
-                                    </div>
-                                </div>
+               <div className="container p-3 card mb-3 shadow border-0">
+               <Slider {...settings}>
+                   {
+                    loading?(
+                       
+                        Array(5).fill(null).map((_, index) => (
+                            <div key={index} className="slider-item">
+                               <Shimmer width="100%" height={300} className='rounded-3 mb-3'/>
+                               <Shimmer width="60%" height={20} className='mb-2'/>
+                               <Shimmer width="80%" height={10} className='mb-2'/>
+                               <Shimmer width="70%" height={10} className='mb-2'/>
+                               <Shimmer width="60%" height={10} className='mb-2'/>
                             </div>
-                        </div>
-                    ))}
+                        ))
+                    ):(
+                        BlogsData.map((item, index) => (
+                            <div key={index} className="slider-item">
+                               <img src={item.image} alt="" className="w-100 top-blog-image" />
+                               <h1 className="fs-5 fw-bold mt-3">{item.title}</h1>
+                               <i className="fs-6">{item.subtitle}</i>
+                            </div>
+                        ))
+                    )
+                   }
                 </Slider>
+               </div>
             </div>
-            <div className="all-blogs p-lg-5 py-1 p-2">
+            <div className="all-blogs p-2">
+                <div className="container">
                 <div className="row">
-                    <div className="col-12 col-md-8 col-lg-8">
-                        <div className="blogs-all-left">
                             {
-                                BlogsData.map((item, index)=>(
-                                    <div className="big-blog-card border rounded-5 p-3 bg-white mb-3">
-                                        <div className="row">
-                                            <div className="col-12 col-md-4 col-lg-5 d-flex align-items-center">
-                                                <img src={item.image} alt="blog" className="w-100 rounded-3" />
-                                            </div>
-                                            <div className="col-12 col-md-8 col-lg-7">
-                                                <p className="fs-5 text-main-danger">{item.tag}</p>
-                                                <h1 className="fs-2 text-900">{item.title}</h1>
-                                                <p className="subtitle"><ReadMore text={item.subtitle} maxLength={100}/></p>
-                                                <div className="d-flex flex-wrap gap-2">
-                                            <p className="fs-5"><i class="bi bi-calendar3 text-yellow mx-2"></i>  {new Date(item.Date).toDateString()}</p>
-                                                <p className="fs-5"><i class="bi bi-alarm text-green mx-2"></i> {new Date(item.Date).toLocaleTimeString()}</p>
-                                            </div>
-                                           <div className="text-end">
-                                           <Link to={`/Blog/read/${item._id}`} className='text-main-danger fs-4'>Read Blog</Link>
+                               loading?(
+                                Array(6).fill(null).map((_, index)=>(
+                                    <div className="col-md-6">
+                                    <div className="card shadow border rounded-2 p-2 bg-white mb-3 border-0">
+                                      <Shimmer width="100%" height={300} className='mb-3 rounded-3'/>
+                                      <Shimmer width="100px" height={15} className='mb-2'/>
+                                      <Shimmer width="70%" height={20} className='mb-2'/>
+                                      <Shimmer width="80%" height={10} className='mb-2'/>
+                                               
+                                               <div className="d-flex flex-wrap gap-2 mb-4">
+                                           <Shimmer width="100%" height={10}/>
+                                           <Shimmer width="100%" height={10} />
                                            </div>
+                                          <div className="text-end">
+                                        
+                                          <Shimmer width="30%" height={20} className='mb-3 rounded-3'/>
+
+                                          </div>
+                                   </div>
+                                  </div>
+                                 ))
+                               ):(
+                                BlogsData.map((item, index)=>(
+                                
+                                    <div className="col-md-6">
+                                      <div className="card border shadow rounded-2 p-0 bg-white mb-3 overflow-hidden border-0">
+                                      <div className="card-header p-0">
+                                      <img src={item.image} alt="" className="w-100" />
+                                      </div>
+                                               <div className="card-body">
+                                               <p className="fs-5 text-main-danger">{item.tag}</p>
+                                                 <h1 className="fs-4 fw-bold">{item.title}</h1>
+                                                 <i className="lead text-secondary"><ReadMore text={item.subtitle} maxLength={100}/></i>
+                                                 <div className="d-flex flex-wrap gap-2">
+                                             <p className="fs-6 text-secondary"><i class="bi bi-calendar3 text-yellow mx-2"></i>  {new Date(item.Date).toDateString()}</p>
+                                                 <p className="fs-6 text-secondary"><i class="bi bi-alarm text-green mx-2"></i> {new Date(item.Date).toLocaleTimeString()}</p>
+                                             </div>
+                                            <div>
+                                            <Link to={`/Blog/read/${item._id}`} className='btn btn-outline-dark fs-5 border px-5 rounded-pill'>Read Blog</Link>
                                             </div>
-                                        </div>
+                                               </div>
+                                     </div>
                                     </div>
-                                ))
+                                 ))
+                               )
                             }
                         </div>
-                    </div>
-                    <div className="col-12 col-md-4 col-lg-4">
-                    <div className="blog-sticky-card p-lg-2 p-4">
-                    <BookDemoCard/>
-                    </div>
-                    </div>
                 </div>
             </div>
         </section>
