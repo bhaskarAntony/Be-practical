3d90d413a88292895fe9ad462a859e10dd2ecc65
bpdeveloperscommunity <bepracticaldeveloperscommunity@gmail.com>
2024-02-20 16:28:16 +0530

    updated files

--- src/pages/BlogsPage/BlogsView.jsx
+++ src/pages/BlogsPage/BlogsView.jsx
@@ -5,16 +5,19 @@ import { useEffect } from 'react';
 import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import BookDemoCard from '../../components/BookDemoCard';
+import { BlogsApi } from '../../Data/DataFetcher';
+import ReadMore from '../../Extra/ReadMore';
 
 function BlogsView() {
     const { id } = useParams(); // Access the id parameter from the URL
     const [BlogData, setBlogData] = useState([]);
+    const [MoreBlogData, setMoreBlogData] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(`https://fancy-mittens-ray.cyclic.app/api/blogs/${id}`);
+        const response = await axios.get(`http://localhost:3300/api/blogs/${id}`);
         setBlogData(response.data);
         setLoading(false);
       } catch (error) {
@@ -22,35 +25,47 @@ function BlogsView() {
         setLoading(false);
       }
     };
+   
 
     // Call the fetchData function when the component mounts
     fetchData();
   }, []); // The empty dependency array ensures the effect runs only once
+  useEffect(()=>{
+    BlogsApi
+    .then((data) => {
+      setMoreBlogData(data)
+    })
+    .catch((error) => console.error('Error fetching advantages:', error));
+  },[])
   return (
     <section className='read-blog container-fluid container-xl'>
-       <div className="bg-blur">
-       <div className="p-lg-5 p-3 text-center">
-        <h1 className="fs-1 text-900">{BlogData.title}</h1>
-        </div>
-        <div className="row">
-            <div className="col-md-6 offset-md-3">
-              
-                <img src={BlogData.image} alt="blog" className="w-100" />
+  
+      
+      <div className="container">
+    <div className="border p-3 mb-3">
+    <img src={BlogData.image} alt="blog" className="w-100" />
+      <h1 className="fs-1 fw-bold mt-2">{BlogData.title}</h1>
+      <h1 className="fs-6">{BlogData.subtitle}</h1>
+    </div>
+      <div className='fs-5 main-blog' dangerouslySetInnerHTML={{ __html: BlogData.content}} />
+    
+      </div>
+      <hr />
+      <h1 className="fs-1 fw-bold mt-5 mb-3">Related Blogs</h1>
+      <div className="moreblogs-container py-5">
+        {
+          MoreBlogData.map((item, index)=>(
+            <div className="card shadow overflow-hidden">
+            <div className="card-header p-0">
+              <img src={item.image} alt={item.title} className="w-100" />
             </div>
-        </div>
-       </div>
-      <div className="row">
-        <div className="col-12 col-md-12 col-lg-8">
-                <div className="blog-container p-lg-5 p-3">
-                  
-                <div className='fs-5' dangerouslySetInnerHTML={{ __html: BlogData.content}} />
-                </div>
-        </div>
-        <div className="col-12 col-md-12 col-lg-4">
-            <div className="blog-sticky-card p-lg-2 p-4">
-                <BookDemoCard/>
+            <div className="card-body">
+              <h1 className="fs-5 fw-bold">{item.title}</h1>
+              <ReadMore text={item.subtitle} maxLength={200} />
             </div>
-        </div>
+          </div>
+          ))
+        }
       </div>
     </section>
   )
