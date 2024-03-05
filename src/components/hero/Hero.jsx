d99c813d9cc170d6c8444d711ffdc579df1c06f8
bpdeveloperscommunity <bepracticaldeveloperscommunity@gmail.com>
2024-03-05 20:27:32 +0530

    all files

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -109,13 +109,13 @@ function Hero() {
             </div>
           </div>
           <div className="weareon d-flex gap-3 mt-3 justsify-content-center align-items-center">
-            <div className="google" data-aos="zoom-in-up">
+            <div className="google w-img" data-aos="zoom-in-up">
              <img src="https://upload.wikimedia.org/wikipedia/en/d/d7/Deccan_Herald_logo.svg" alt="we are on deccan-herald" />
             </div>
-            <div className="google" data-aos="zoom-in-up">
+            <div className="google w-img" data-aos="zoom-in-up">
               <img src="https://bepractical.s3.us-east-2.amazonaws.com/times-logo.png" alt="we are on hindustan times" />
             </div>
-            <div className="google" data-aos="zoom-in-up">
+            <div className="google w-img" data-aos="zoom-in-up">
              <img src="https://bepractical.s3.us-east-2.amazonaws.com/mid-day-logo.png" alt="we are on mid-day" />
             </div>
           </div>
